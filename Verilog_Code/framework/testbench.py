183c3e543c76279070709ef36d1d21fd54f2f88a
Jageshwar <kumarjageshwar007t7@gmail.com>
2026-05-27 14:10:26 +0530

    used python for verification.

---
+++ Verilog_Code/framework/testbench.py
@@ -0,0 +1,38 @@
+import cocotb
+from cocotb.triggers import Timer
+import random 
+import pandas as pd
+
+# Clock;
+def clock(clk):
+    if(clk==0):
+        clk = 1
+    else:
+        clk = 0
+    
+    return clk
+
+@cocotb.test()
+async def GELU(dut):
+    # Initial values:-
+    clk = 1
+
+    i=-512
+    while(i<=512):
+        # Input signals:-
+        clk = clock(clk)
+        num = i
+
+        # Ports:-
+        dut.clk.value = clk
+        dut.num.value = num
+
+        await Timer(5,unit='ps')
+
+        out = dut.out.value
+        print("input=",num," Output=",out)
+        i=i+1
+
+
+
+
